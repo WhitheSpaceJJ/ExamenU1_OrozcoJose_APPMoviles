b52d581707c8c59f84120a637afa83e68df4d0ea
Jose\Jose <jose.orozco229141@potros.itson.edu.mx>
2023-02-12 02:13:53 -0700

    Ya hasta que la compu se dejo

--- ExamenU1/app/src/main/java/mx/edu/itson/examenu1/MainActivity.kt
+++ ExamenU1/app/src/main/java/mx/edu/itson/examenu1/MainActivity.kt
@@ -10,152 +10,120 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val txError: TextView =findViewById(R.id.txtError)
-        val etC: EditText =findViewById(R.id.etC)
-        val etF: EditText =findViewById(R.id.etF)
-        val etK: EditText =findViewById(R.id.etK)
+        val txError: TextView = findViewById(R.id.txtError)
+        val etC: EditText = findViewById(R.id.etC)
+        val etF: EditText = findViewById(R.id.etF)
+        val etK: EditText = findViewById(R.id.etK)
         val btnC: Button = findViewById(R.id.btnGC)
         val btnF: Button = findViewById(R.id.btnGF)
         val btnK: Button = findViewById(R.id.btnGK)
         btnC.setOnClickListener {
-          txError.setText("")
-           if(etC.toString().isEmpty()==false || etF.toString().isEmpty()==false || etK.toString().isEmpty()==false){
-               var celsius:Double=0.0
-               var kelvin:Double=0.0
-               var fahrenheit:Double=0.0
-               try{
-                   celsius=etC.text.toString().toDouble()
-               }catch(e:java.lang.Exception){
-               }
-               if(celsius==0.0){
-                   try{
-                       kelvin=etK.text.toString().toDouble()
-                   }catch(e:java.lang.Exception){
-                   }
-                   try{
-                       fahrenheit=etF.text.toString().toDouble()
-                   }catch(e:java.lang.Exception){
-                   }
-                   if(kelvin!=0.0 && fahrenheit!=0.0){
-                       txError.setText("Solo se requiere que ingrese un valor  kelvin o fahrenheit")
-                   }else{
-                       if(kelvin==0.0 && fahrenheit!=0.0 ){
-                         etC.setText("%.4f".format(calcularFahrenheitToCelsius(fahrenheit)))
-                       }else if(kelvin!=0.0 && fahrenheit==0.0 ){
-                           etC.setText("%.4f".format(calcularKelvinToCelsius(kelvin)))
-                       }
-                   }
-               }else{
-                   txError.setText("No se puede realizar una conversion de celsius a celsius")
-               }
-           }else{
-               txError.setText("Se requiere que ingrese algun valor antes de realizar alguna conversion")
-           }
+            txError.setText("")
+            if (etC.text.toString().isEmpty() == false || etF.text.toString()
+                    .isEmpty() == false || etK.text.toString().isEmpty() == false
+            ) {
+                var kelvin: Double = 0.0
+                var fahrenheit: Double = 0.0
+                if (etC.text.toString().isEmpty()) {
+                    try {
+                        kelvin = etK.text.toString().toDouble()
+                    } catch (e: java.lang.Exception) {
+                    }
+                    try {
+                        fahrenheit = etF.text.toString().toDouble()
+                    } catch (e: java.lang.Exception) {
+                    }
+                    if (kelvin != 0.0 && fahrenheit != 0.0) {
+                        txError.setText("Solo se requiere que ingrese un valor  kelvin o fahrenheit")
+                    } else {
+                        if (kelvin == 0.0 && fahrenheit != 0.0) {
+                            etC.setText("%.4f".format(((fahrenheit - 32) / (1.8))))
+                        } else if (kelvin != 0.0 && fahrenheit == 0.0) {
+                            etC.setText("%.4f".format((kelvin - 273.15)))
+                        }
+                    }
+                } else {
+                    txError.setText("No se puede realizar una conversion de celsius a celsius")
+                }
+            } else {
+                txError.setText("Se requiere que ingrese algun valor antes de realizar alguna conversion")
+            }
         }
         btnK.setOnClickListener {
             txError.setText("")
-            if(etC.toString().isEmpty()==false || etF.toString().isEmpty()==false || etK.toString().isEmpty()==false){
-                var celsius:Double=0.0
-                var kelvin:Double=0.0
-                var fahrenheit:Double=0.0
+            if (etC.text.toString().isEmpty() == false || etF.text.toString()
+                    .isEmpty() == false || etK.text.toString().isEmpty() == false
+            ) {
+                var celsius: Double = 0.0
+                var kelvin: Double = 0.0
+                var fahrenheit: Double = 0.0
 
-                try{
-                    kelvin=etK.text.toString().toDouble()
-                }catch(e:java.lang.Exception){
+                try {
+                    kelvin = etK.text.toString().toDouble()
+                } catch (e: java.lang.Exception) {
                 }
 
-                if(kelvin==0.0){
-                    try{
-                        fahrenheit=etF.text.toString().toDouble()
-                    }catch(e:java.lang.Exception){
+                if (kelvin == 0.0) {
+                    try {
+                        fahrenheit = etF.text.toString().toDouble()
+                    } catch (e: java.lang.Exception) {
                     }
-                    try{
-                        celsius=etC.text.toString().toDouble()
-                    }catch(e:java.lang.Exception){
+                    try {
+                        celsius = etC.text.toString().toDouble()
+                    } catch (e: java.lang.Exception) {
                     }
-                    if(celsius!=0.0 && fahrenheit!=0.0){
+                    if (celsius != 0.0 && fahrenheit != 0.0) {
                         txError.setText("Solo se requiere que ingrese un valor  celsius o fahrenheit")
-                    }else{
-                        if(celsius==0.0 && fahrenheit!=0.0 ){
-                            etK.setText("%.4f".format(calcularFahrenheitToKelvin(fahrenheit)))
-                        }else if(celsius!=0.0 && fahrenheit==0.0 ){
-                            etK.setText("%.4f".format(calcularCelsiusToKelvin(celsius)))
+                    } else {
+                        if (celsius == 0.0 ) {
+var respuesta:Double=(fahrenheit - 32) * 5 / 9 + 273.15
+                            etK.setText(String.format("%.4f",(respuesta)))
+                        } else if (celsius != 0.0) {
+                            etK.setText("%.4f".format(celsius + 273.15))
                         }
                     }
-                }else{
+                } else {
                     txError.setText("No se puede realizar una conversion de kelvin a kelvin")
                 }
-            }else{
+            } else {
                 txError.setText("Se requiere que ingrese algun valor antes de realizar alguna conversion")
             }
         }
         btnF.setOnClickListener {
             txError.setText("")
-            if(etC.toString().isEmpty()==false || etF.toString().isEmpty()==false || etK.toString().isEmpty()==false){
-                var celsius:Double=0.0
-                var kelvin:Double=0.0
-                var fahrenheit:Double=0.0
+            if (etC.text.toString().isEmpty() == false || etF.text.toString()
+                    .isEmpty() == false || etK.text.toString().isEmpty() == false
+            ) {
+                var celsius: Double = 0.0
+                var kelvin: Double = 0.0
 
-                try{
-                    fahrenheit=etF.text.toString().toDouble()
-                }catch(e:java.lang.Exception){
-                }
-
-                if(fahrenheit==0.0){
-                    try{
-                        kelvin=etK.text.toString().toDouble()
-                    }catch(e:java.lang.Exception){
+                if (etF.text.toString().isEmpty()) {
+                    try {
+                        kelvin = etK.text.toString().toDouble()
+                    } catch (e: java.lang.Exception) {
                     }
-                    try{
-                        celsius=etC.text.toString().toDouble()
-                    }catch(e:java.lang.Exception){
+                    try {
+                        celsius = etC.text.toString().toDouble()
+                    } catch (e: java.lang.Exception) {
                     }
-                    if(celsius!=0.0 && kelvin!=0.0){
+                    if (celsius != 0.0 && kelvin != 0.0) {
                         txError.setText("Solo se requiere que ingrese un valor  celsius o kelvin")
-                    }else{
-                        if(celsius==0.0 && kelvin!=0.0 ){
-                            etF.setText("%.4f".format(calcularKelvinToFahrenheit(kelvin)))
-                        }else if(celsius!=0.0 && kelvin==0.0 ){
-                            etF.setText("%.4f".format(calcularCelsiusToFahrenheit(celsius)))
+                    } else {
+                        if (celsius == 0.0 && kelvin != 0.0) {
+                            var result =( (kelvin - 273.15) * (9 ))/ 5 + 32
+                            etF.setText("%.4f".format(result))
+                        } else if (celsius != 0.0 && kelvin == 0.0) {
+                            var result = celsius * 9 / 5 + 32
+                            etF.setText("%.4f".format(result))
                         }
                     }
-                }else{
+                } else {
                     txError.setText("No se puede realizar una conversion de fahrenheit a fahrenheit")
                 }
-            }else{
+            } else {
                 txError.setText("Se requiere que ingrese algun valor antes de realizar alguna conversion")
             }
         }
     }
-    
-//Tampoco
-    fun calcularFahrenheitToCelsius(fahrenheit:Double): Double{
-       var base:Double=(fahrenheit-32)*(5/9)
-        return (base)
-    }
 
-    //Tampoco
-    fun calcularKelvinToCelsius(kelvin:Double):Double{
-        var  base:Double=(kelvin-273.15)
-return (base)
-    }
-//NoFunciona
-    fun calcularCelsiusToFahrenheit(celsius:Double): Double{
-    var base:Double=celsius*(9/5)
-        return (base+32)
-    }
-    //Tampoco
-    fun calcularKelvinToFahrenheit(kelvin:Double):Double{
-        var base:Double=(kelvin-273.15)*(9/5)
-     return (base+32)
-    }
-    fun calcularCelsiusToKelvin(celsius:Double):Double{
-        var base:Double=(celsius+273.15)
-        return (base)
-    }
-    //Tampocoyyyyyy
-    fun calcularFahrenheitToKelvin(fahrenheit:Double):Double{
-        var base:Double=(fahrenheit-32)*(5/9)
-        return (base+273.15)
-    }
 }
